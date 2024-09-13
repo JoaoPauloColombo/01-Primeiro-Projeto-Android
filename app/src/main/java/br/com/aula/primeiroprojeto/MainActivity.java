8f4b67b35ecd85740da06733eba21524d0c868de
Jo�o Paulo Souza Colombo <23025476@fecap.br>
2024-09-12 21:51:24 -0300

    Primeiro Commit

---
+++ app/src/main/java/br/com/aula/primeiroprojeto/MainActivity.java
@@ -0,0 +1,35 @@
+package br.com.aula.primeiroprojeto;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void alterarTexto(View view){
+        TextView resultado = findViewById(R.id.textoResultado);
+        resultado.setText("Voce clicou!!!!");
+    }
+    public void deletarTexto(View view){
+        TextView resultado = findViewById(R.id.textoResultado);
+        resultado.setText("");
+    }
+}
